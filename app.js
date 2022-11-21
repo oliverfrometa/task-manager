0dc64750f02afd3bfee6e008e7a680ebef553f03
Oliver <oliverfrometa17@gmail.com>
2022-11-21 12:08:37 -0500

    MODAL

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const tasksRemaining = document.querySelector('#tasksRemaining');
 
-tasksRemaining.innerHTML = `You have 1 task remaining`
+tasksRemaining.innerHTML = `You have 1 task remaining`;
+
+// ------------------------------ MODALS
+
+const tasksModalOpen = document.querySelector('#tasksModalOpen');
+const tasksModalContainer = document.querySelector('#tasksModalContainer');
+const tasksModalClose = document.querySelector('#tasksModalClose');
+
+tasksModalOpen.addEventListener('click', () => {
+    tasksModalContainer.classList.add('show')
+});
+
+tasksModalClose.addEventListener('click', () => {
+    tasksModalContainer.classList.remove('show')
+});
